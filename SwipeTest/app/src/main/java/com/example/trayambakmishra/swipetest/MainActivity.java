5d8994ab0c1ccd0178e5af9143ad6cfb79741eaf
trayambakm <trayambakm@mindfiresolutions.com>
2015-05-06 15:37:46 +0530

    Adding Swipe Test folder for the first time
    Signed-off-by: trayambakm <trayambakm@mindfiresolutions.com>

---
+++ SwipeTest/app/src/main/java/com/example/trayambakmishra/swipetest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.trayambakmishra.swipetest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Trayambak Mishra on 4/7/2015.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
